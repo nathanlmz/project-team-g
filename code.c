e93edd0af3b7cc44e46b0dbefb50137196a97629
EthanNing <36296773+EthanNing@users.noreply.github.com>
2019-04-20 23:24:03 +0800

    Update code.c
    Simple base 10 to base <10 converters.

--- code.c
+++ code.c
@@ -1,1 +1,21 @@
+/*
+** Simple base 10 to base <10 converters.
+*/
 
+#include <stdio.h>
+
+main(int argc, char**argv)
+   {
+   if (argc == 3)
+      lastdigit(atoi(*++argv), atoi(*++argv));
+   else
+      printf("Usage: %s newbase", *argv);
+   putchar('\n');
+   }
+
+lastdigit(int num, int base)
+   {
+   if (num >= base)
+      lastdigit(num/base, base);
+   putchar(num%base + '0');
+   }
